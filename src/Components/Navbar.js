e4543bd3681e50a52e298ab1b587b4b3e3e0406a
lakshitGarg539 <lakshitgarg539@gmail.com>
2022-08-21 12:20:18 +0530

    responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,34 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import Input from './Input';
 import logo from '../Assets/logo.png';
+
 export default function Navbar() {
   const [searchterm, setsearchterm] = useState("");
   return <>
-    <nav class="navbar navbar-dark bg-dark">
-    <div className="container-fluid">
-      <div>
-    <img src={logo} alt="" style={{width:"1.7em"}} />
-    <a className="navbar-brand px-2" href="#">To-Do</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={logo} width="30" height="30" class="d-inline-block align-top mx-1" alt="" />
+          To-Do
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page">Home</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e)=>{
+              setsearchterm(e.target.value);
+            }} />
+          </form>
+        </div>
       </div>
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={(e)=>{setsearchterm(e.target.value)}}/>
-    </form>
-  </div>
-</nav>
+    </nav >
 
-  <Input searchterm = {searchterm}/>
+    <Input searchterm={searchterm} />
   </>
 }
